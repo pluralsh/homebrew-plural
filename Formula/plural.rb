9d650ba792a6a65b23682e7ed875337fcfe891ca
michaelguarino <mguarino46@gmail.com>
2021-09-22 19:09:55 -0400

    Initial tap for plural

---
+++ Formula/plural.rb
@@ -0,0 +1,25 @@
+class Plural < Formula
+  desc "CLI for interacting with the plural platform"
+  homepage "https://docs.plural.sh"
+  url "https://github.com/pluralsh/plural-cli.git",
+    revision: "bdba2565eb8cdb7a87223b0dd7d6d40f36cccd3e"
+  license "GNU Affero General Public License v3.0"
+  head "https://github.com/pluralsh/plural-cli.git"
+
+  depends_on "helm"
+  depends_on "kubectl"
+  depends_on "go" => :build
+
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    system "make", "build-cli"
+    bin.install "plural.o" => "plural"
+  end
+
+  test do
+    # ensure plural --help runs correctly
+    system bin/"plural", "--help"
+  end
+end
