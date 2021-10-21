7fd2347a3be9e26bc8e211ebc038291bccc845ef
michaelguarino <mguarino46@gmail.com>
2021-10-21 12:31:24 -0400

    bump version

--- Formula/plural.rb
+++ Formula/plural.rb
@@ -2,8 +2,8 @@ class Plural < Formula
   desc "CLI for interacting with the plural platform"
   homepage "https://docs.plural.sh"
   url "https://github.com/pluralsh/plural-cli.git",
-    revision: "870c9fe5d68f6497c2f3deb2e82bfc387358ccfa"
-  version "0.1.10"
+    revision: "34b351f57fd8b665621e36e22a9e2c344d6b1bf1"
+  version "0.1.11"
   license "GNU Affero General Public License v3.0"
   head "https://github.com/pluralsh/plural-cli.git"
 
