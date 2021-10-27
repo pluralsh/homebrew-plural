35b41e1c3186eec23fbf4b57bb4a0c877d6d708f
michaelguarino <mguarino46@gmail.com>
2021-10-27 18:38:30 -0400

    add helm plugins

--- Formula/plural.rb
+++ Formula/plural.rb
@@ -3,7 +3,7 @@ class Plural < Formula
   homepage "https://docs.plural.sh"
   url "https://github.com/pluralsh/plural-cli.git",
     revision: "bb43da06badea3e4e5b7479bc0da3497d1331763"
-  version "0.1.12"
+  version "0.1.13"
   license "GNU Affero General Public License v3.0"
   head "https://github.com/pluralsh/plural-cli.git"
 
@@ -16,6 +16,8 @@ class Plural < Formula
     # Don't dirty the git tree
     rm_rf ".brew_home"
 
+    system "helm", "plugin", "install" , "https://github.com/pluralsh/helm-push"
+    system "helm", "plugin", "install", "https://github.com/databus23/helm-diff"
     system "make", "build-cli"
     bin.install "plural.o" => "plural"
   end
