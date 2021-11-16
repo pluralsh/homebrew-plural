36413c07555b285791dad72196f6f7efd6623522
michaelguarino <mguarino46@gmail.com>
2021-11-16 09:22:58 -0500

    don't install helm-diff by default

--- Formula/plural.rb
+++ Formula/plural.rb
@@ -2,8 +2,8 @@ class Plural < Formula
   desc "CLI for interacting with the plural platform"
   homepage "https://docs.plural.sh"
   url "https://github.com/pluralsh/plural-cli.git",
-    revision: "be1cba00abaad6d34380ec776b718d3cbe3992e6"
-  version "0.1.21"
+    revision: "ed65914b87c6aec914b5aebbf423572669f992cc"
+  version "0.1.22"
   license "GNU Affero General Public License v3.0"
   head "https://github.com/pluralsh/plural-cli.git"
 
@@ -17,7 +17,7 @@ class Plural < Formula
     rm_rf ".brew_home"
 
     system "helm", "plugin", "install", "https://github.com/pluralsh/helm-push"
-    system "helm", "plugin", "install", "https://github.com/databus23/helm-diff"
+    # system "helm", "plugin", "install", "https://github.com/databus23/helm-diff"
     system "make", "build-cli"
     bin.install "plural.o" => "plural"
   end
